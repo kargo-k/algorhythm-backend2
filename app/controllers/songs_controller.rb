9c8fb7f483a0bfd047141749d68cde67fb2ed70e
kargo <karenrgo@gmail.com>
2019-08-15 23:41:02 -0700

    added index and show json renders for playlists and songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,13 @@
 class SongsController < ApplicationController
+
+    def index
+        songs = Song.all
+        render json: songs
+    end
+
+    def show
+        song = Song.find(params[:id])
+        render json: song
+    end
+    
 end
